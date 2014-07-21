14d2408dae866363a88fb9cfadd164c2d2536169
IGlautier <ivanglautier@gmail.com>
2014-07-21 13:59:34 +0100

    Concatenation script
    Beginning of js script to concatenate based on index.md

---
+++ js/concat.js
@@ -0,0 +1,36 @@
+var existing;
+
+getText = function(url, callback) {
+    var request = new XMLHttpRequest();
+	
+    request.onreadystatechange = function()
+    {
+        if (request.readyState == 4 && request.status == 200)
+        {
+            callback(request.responseXML); 
+        }
+    }; 
+    request.open('GET', url);
+	request.responseType = "document";
+    request.send();
+}
+
+
+function getLinks(content) {
+	links = document.getElementsByTagName('a');
+	for(var i = 0; i < links.length; i++) {
+		if(typeof existing[links[i].href] == "undefined") {
+			existing[links[i].href] = false;
+			/*check if a local link*/
+			getText(links[i].href, function(res) {
+				if (res.title == "index.md") getLinks(res);
+				var newContent = document.createElement("div");
+				newContent.innerHTML = res.;
+				links[i].href.parentNode.appendChild(newContent);
+			});
+			
+		}
+	}
+}
+
+
