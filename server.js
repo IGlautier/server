1805a7ececc082fcbf110b5f25b5bc27aa34bbc4
IGlautier <ivanglautier@gmail.com>
2014-07-21 10:27:12 +0100

    Better Error Handling
    Not sure if this is how you're supposed to handle errors, but it
    works...

--- server.js
+++ server.js
@@ -12,20 +12,14 @@ app.get("/", function(req, res) {
 		console.log(__dirname + "/index.md");
 		if(err) {
 			if(err.code == "ENOENT")	res.send("404 - not found");
-			throw err;
-		}		
-		template.templatemd(data, function(output) {
-			util.pump(output,res);
-		});
-		
+			else throw err;
+		}
+		else {
+			template.templatemd(data, function(output) {
+				util.pump(output,res);
+			});
+		}	
 	});	
-});
-
-
-
-app.post("/", function(req, res) { 
-	/* some server side logic */
-	res.send("OK");
 });
 
 /* serves all the md files */
@@ -36,21 +30,57 @@ app.get("*.md", function(req, res) {
 		console.log(__dirname + req.params[0]+".md");
 		if(err) {
 			if(err.code == "ENOENT")	res.send("404 - not found");
-			throw err; 
+			else throw err; 
 		}
-		template.templatemd(data, function(output) {
-			util.pump(output,res);
-		});
-		
+		else {
+			template.templatemd(data, function(output) {
+				util.pump(output,res);
+			});
+		}	
 	});				
 });
+
+app.post("/", function(req, res) { 
+	/* some server side logic */
+	res.send("OK");
+});
+
+
 
 
 
 /* serves all the static files and index files*/
 app.get(/^(.+)$/, function(req, res){ 
 	console.log('static file request : ' + util.inspect(req.params, false, null));
-	fs.lstat(__dirname + req.params[0], router.directory(__dirname + req.params[0], function(isDir) {
+	fs.readFile(__dirname + req.params[0], "utf8", function(err,data) {
+		if(err) {
+		
+			switch(err.code) {
+				case "ENOENT":
+					res.send("404 - not found");
+					break;
+				case "EISDIR":
+					fs.readFile(__dirname + req.params[0]+"/index.md", "utf8", function(err,data) {
+						if(err) {
+							if(err.code == "ENOENT")	res.send("404 - not found");
+							else throw err; 
+						}
+						else {
+							template.templatemd(data, function(output) {
+								util.pump(output,res);
+							});
+						}	
+					});
+					break;
+				default:
+					throw err;
+			}
+		}
+		else {	
+			res.sendfile(__dirname + req.params[0]);
+		}
+	});
+	/*fs.lstat(__dirname + req.params[0], router.directory(__dirname + req.params[0], function(isDir) {
 		if (isDir) 
 			fs.readFile(__dirname + req.params[0]+"/index.md", "utf8", function(err,data) {
 			console.log(__dirname + req.params[0]+"/index.md");
@@ -61,7 +91,7 @@ app.get(/^(.+)$/, function(req, res){
 		
 		});
 		else res.sendfile(__dirname + req.params[0]);	
-	}));
+	}));*/
 });
 
 
